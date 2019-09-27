c4f503b6cc46da447783e63b4a1a9063a65c0237
Dainius <dainius.kreivenas@gmail.com>
2019-09-27 22:36:05 +0100

    Assoc relationships tests

--- quillianYes.py
+++ quillianYes.py
@@ -5,7 +5,7 @@ Simple test for monkey reach using associations
 """
 import pyNN.nest as sim
 from rbs import FSAHelperFunctions
-from rbs import Association
+from rbs import AssociationBuilder
 from rbs import RuleBasedSystemBuilder
 
 sim.setup(timestep=1.0,min_delay=1.0,max_delay=1.0, debug=0)
@@ -14,9 +14,10 @@ simTime = 100
 
 fsa = FSAHelperFunctions(sim, "nest")
 
-association = Association(sim, "nest")
-association.useBases("bases")
-association.build()
+associationBuilder = AssociationBuilder(sim, "nest")
+associationBuilder.useBases("bases")
+associationBuilder.useRelationships("props", "rels", "assocs")
+association = associationBuilder.build()
 
 rbsBuilder = RuleBasedSystemBuilder(sim, "nest", fsa)
 rbsBuilder.useAssociation(association)
@@ -62,7 +63,41 @@ rbs.addRule(
     ]
 )
 
-rbs.addFact("question", ("is", "canary", "animal"))
+rbs.addRule(
+    "hasBaseOperationProperty",
+    [
+        (True, "question", ("does", "?base", "?rel", "?property"), "f1")
+    ],
+    [
+        ("link", ("base", "?base", "stimulate")),
+        ("link", ("relationship", "?rel", "stimulate")),
+        ("prime", "property")
+    ]
+)
+
+rbs.addRule(
+    "resolveBaseOperationProperty",
+    [
+        (True, "question", ("does", "?base", "?rel", "?property"), "f1"),
+        ("property", "?property", "f2"),
+        ("link", ("base", "?base", "stimulate"), "f3"),
+        ("base", "?base", "f4"),
+        ("link", ("relationship", "?rel", "stimulate"), "f5"),
+        ("relationship", "?rel", "f6"),
+        ("prime", "property", "f7")
+    ],
+    [
+        ("assert", ("is", ("?base", "?rel", "?property"))),
+        ("retract", "f1"),
+        ("retract", "f3"),
+        ("retract", "f5"),
+        ("retract", "f7"),
+    ]
+)
+
+rbs.addFact("question", ("does", "canary", "eats", "food"))
+
+#rbs.addFact("question", ("is", "canary", "animal"))
 
 ## Run
 sim.run(simTime)
